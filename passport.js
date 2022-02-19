5499b52c013e920e85376d1f48263766ebb38fc4
ThoArrow <thoarrown@gmail.com>
2022-02-19 20:44:23 +0700

    fix client url

--- passport.js
+++ passport.js
@@ -4,13 +4,13 @@ require("dotenv").config();
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-
+const clientUrl = process.env.CLIEN_URL;
 passport.use(
   new GoogleStrategy(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:5001/auth/google/callback",
+      callbackURL: clientUrl + "/auth/google/callback",
       passReqToCallback: true,
     },
     function (request, accessToken, refreshToken, profile, done) {
